22f27a612934b06c1a68b1e4df39edcac70ed601
Qin Chen <qchen8@sfsu.edu>
2023-11-04 23:41:19 -0700

    Creat Qin's files

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -5,6 +5,8 @@ import { List, Map } from 'immutable';
 import { PianoInstrument } from './instruments/Piano';
 import { WaveformVisualizer } from './visualizers/Waveform';
 
+import { QCInstrument } from './instruments/qchen1218';
+import { QCVisualizer } from './visualizers/qchen1218';
 
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
@@ -15,13 +17,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, QCInstrument]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer]);    // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer, QCVisualizer]);    // similar to Visualizer[]
 
 
 /**
