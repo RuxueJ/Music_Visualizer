f222f1c1a10de0488a8784d9f78f31b8861bcc29
Qin Chen <111808135+qchen1218@users.noreply.github.com>
2023-11-28 22:36:42 -0800

    tried to add a new song

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -1,11 +1,20 @@
-CREATE TABLE IF NOT EXISTS songs (
+CREATE TABLE songs (
 	id int NOT NULL PRIMARY KEY,
 	song_title text NOT NULL,
-	instrument varchar NOT NULL,
 	notes varchar NOT NULL,
-	imageLink varchar
+	instrument text,
+	image_link text,
+	author text,
+	genre text,
+	public_time text
 );
 
-INSERT OR IGNORE INTO songs (id, song_title, instrument, notes) 
--- VALUES (1, 'Ode to Joy (Dubstep Remix)',  'piano','E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
-VALUES (1, 'Ode to Joy (Dubstep Remix)',  'piano','D4 E4 E4 D4 D4');
+INSERT INTO songs (id, song_title, notes) 
+VALUES 
+(1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4'), 
+(2, 'some notes', 'D4 E4 E4 D4 D4');
+
+
+
+--  (1, 'Ode to Joy (Dubstep Remix)', 'piano', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', './image/id_1.PNG', 'N/A', 'Remix', 'N/A'),
+--   (2, 'Ode to Joy (Dubstep Remix)', 'piano', 'D4 E4 E4 D4 D4', './image/id_1.PNG', 'sample', 'testing', 'test');
