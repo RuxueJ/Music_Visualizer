03decaf1508f568bd0d4836b021f06f592440e0b
Hoang Anh Tran <htran31@sfsu.edu>
2023-11-30 13:23:18 -0800

    fixed the playlists UI and added a cute cat block

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE songs (
 	id int NOT NULL PRIMARY KEY,
-	song_title text NOT NULL,
+	song_title text,
 	notes varchar NOT NULL,
 	instrument text,
 	image_link text,
@@ -11,6 +11,6 @@ CREATE TABLE songs (
 
 INSERT INTO songs (id, song_title, notes, instrument, image_link, author, genre, public_time) 
 VALUES 
-(1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 'piano', './image/id_1.PNG', 'N/A', 'Remix', 'N/A'),
-(2, 'some note', 'D5 E4 E4 D5 D4', 'piano', './image/id_1.PNG', 'sample', 'testing', 'test'),
-(3, 'Twinkle Little Star', 'C5 C5 G5 G5 A5 A5 G5 F5 F5 E5 E5 D5 D5 C5 G5 G5 F5 F5 E5 E5 D5 G5 G5 F5 F5 E5 E5 D5 C5 C5 G5 G5 A5 A5 G5 F5 F5 E5 E5 D5 D5 C5', 'piano', './image/id_1.PNG', 'sample', 'testing', 'test');
+(1, 'Ode to Joy', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 'piano', './image/id_1.PNG', 'N/A', 'Dubstep Remix', 'N/A'),
+(2, 'Twinkle Little Star', 'C5 C5 G5 G5 A5 A5 G5 F5 F5 E5 E5 D5 D5 C5 G5 G5 F5 F5 E5 E5 D5 G5 G5 F5 F5 E5 E5 D5 C5 C5 G5 G5 A5 A5 G5 F5 F5 E5 E5 D5 D5 C5', 'piano', './image/id_1.PNG', 'N/A', 'N/A', 'N/A'),
+(3, 'Testing...', 'D5 E4 E4 D5 D4', 'piano', './image/id_1.PNG', 'test', 'test', 'test');
